269a2c6e863c9b7accd1c1e478832a265c32caf7
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2023-10-08 17:41:23 +0200

    delete unnecessary comment

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-/**
- Challenge:
-
- With the 5 blog post objects, display the `title` and `body`
-properties of the first 5 posts on the browser page.
- 
- Hints: 
- * Create a `div` in the HTML file to store these items
- * Loop over the items creating a string of HTML elements you 
-   can then put into the div with `innerHTML`
- */
-
 let postsArray = [];
 const titleInput = document.querySelector("#post-title-form");
 const bodyInput = document.querySelector("#post-body-form");
